6521bf483462f6efbd77830bb406e1ade43052ba
AlexWetzel <wetzela@live.com>
2018-10-10 17:16:09 -0400

    Added styling to the start page

--- client/src/pages/Start/Start.js
+++ client/src/pages/Start/Start.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './start.css'
 
 class Start extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Start screen</h1>
-        <Link to="/menu"><button>Start</button></Link>
+      <div id="Start" className="row m-0">
+        <div className="col-12 text-center">
+          <h1 className="display-1">Sandwich Store</h1>
+          <Link to="/menu"><div className="start-btn shadow-sm mt-3">Start</div></Link>
+        </div>
       </div>
     )
   }
