61a6d8a7df961ad2b590dc16b381b775d27973a7
AlexWetzel <wetzela@live.com>
2018-11-14 19:34:12 -0500

    Able to get ingredient and sandwich data from database

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,27 @@ const router = require("express").Router();
 const db = require('../models');
 
 router.get("/api/testconnection", (req, res) => {
+  Promise.all([
+    db.Sandwich.findAll({}),
+    db.Ingredient.findAll({})
+  ]).then( data => {
+    // console.log(data[0][0].dataValues);
 
-  db.Sandwich.findAll({}).then( sandwiches => {
-    res.json( sandwiches );
+    const sandwiches = data[0].map( sandwich => {
+      let sandwichData = sandwich.dataValues;
+      return {name: sandwichData.name, price: sandwichData.price};
+    })
+    const ingredients = data[1].map( ingredient => {
+      let ingredientData = ingredient.dataValues;
+      return {name: ingredientData.name, type: ingredientData.type};
+    })
+
+    res.json({
+      sandwiches: sandwiches,
+      ingredients: ingredients
+    });
   })
+
 })
 
 console.log("Routes working");
