46e0193a6e603c655029ebd07ce18c32e0400123
AlexWetzel <wetzela@live.com>
2018-12-14 17:37:48 -0500

    Removed unnecessary old files

--- OLDmodels/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Ingredient: require('./ingredients.js'),
-  Sandwich: require('./sandwiches.js'),
-  User: require('./users.js'),
-  SandwichIngredients: require('./SandwichIngredients.js'),
-  Order: require('./Order.js')
-}
--- OLDmodels/seeds/userSeed.js
+++
@@ -1,7 +0,0 @@
-const User = require('../users');
-
-db.User.sync({force: true}).then(() => {
-  console.log('Synch successful!')
-}).catch( err => {
-  console.log(err);
-});
