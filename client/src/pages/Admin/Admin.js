37ad4175b05ded034c816809a2e2477b88e739b4
AlexWetzel <wetzela@live.com>
2018-12-19 16:11:13 -0500

    Prototype react form working

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -2,13 +2,60 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 
 class Admin extends Component {
+  constructor(props) {
+    super(props);
 
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  state = {
+    username: '',
+    pin: '',
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.username, this.state.pin);
+  }
   
   render() {
     return (
-      <div>
-        <p>test</p>
+      <div className="container">
+        <div className="jumbotron">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">username</label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              className="form-control"
+              onChange={this.handleInputChange} />
+
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">pin</label>
+            <input
+              type="text"
+              name="pin"
+              value={this.state.pin}
+              className="form-control"
+              onChange={this.handleInputChange}/>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
       </div>
     )
   }
