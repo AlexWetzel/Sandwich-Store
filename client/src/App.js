2600d769773d45896978b5bc783beb03a9f484fe
AlexWetzel <wetzela@live.com>
2019-01-16 14:59:19 -0500

    Preparing for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,6 @@ import Admin from './pages/Admin';
 import './App.css';
 import axios from 'axios';
 
-// const AuthState = {
-//   isAuthenticated: false,
-//   login(cb) {
-//     console.log("Authorize state: true");
-//     this.isAuthenticated = true;
-//     return cb;
-//   },
-//   logout(cb) {
-//     this.isAuthenticated = false;
-//     return cb;
-//   }
-// };
-
-// const ProtectedRoute = ({ component: Component, test, ...rest }) => (   
-//   // return(
-//     <Route {...rest} render={(props) => (
-//       AuthState.isAuthenticated === true
-//       ? <Component {...props} test={test} />
-//       : <Redirect to='/' />
-//     )} />
-//   // )
-// )
-
 class App extends Component {
 
   state = {
@@ -36,16 +13,6 @@ class App extends Component {
     inventory: [],
     counter: 0
   }
-
-  // ==App functionality==
-  //1. A start screen prompts the user to begin their order
-  //2. A list of menu items displays different sandwiches that are available. Clicking a sandwich lets you customize with different toppings.
-  //3. After clicking a sandwich, a list of ingredients replaces the menu items. The user can freely toggle which ingredients they desire, and can select a prompt to go to the next page.
-  //4. The ingredients are separated into dofferent pages in this order: Condiments, Cheese, vegetables
-  //5. The order panel on the right side of the page displays the sandwich, with the ingredients under it, that the user has ordered.
-  //6. Once the user copletes the order for one sandwich, they are prompted to either order another sandwich or submit their order
-  //7. A confirmation screen is displayed for the user to confirm or edit their order if needed.
-  //8. After the user submits their order, a thank you message is displayed, and the app returns to the start screen.
 
   componentDidMount() {
     this.getMenuData(() => {});
