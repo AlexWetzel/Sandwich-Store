9796e7b5c7ce25ab279b879f9e6102af30580017
AlexWetzel <wetzela@live.com>
2019-01-16 15:50:07 -0500

    Debugging production build

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 // app.use(cookieParser()); ??
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 // Passport
 // ========================================================
