aeca91f647d1f5ccb3189b9e27b090d57b4e6f5a
AlexWetzel <wetzela@live.com>
2019-01-16 18:04:15 -0500

    Redoing deployment

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const session = require('express-session');
 const passport = require('./passport');
 const user = require('./routes/user.js')(passport);
 const flash = require('connect-flash-plus');
-const app = express();
 const path = require('path');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,9 +15,9 @@ app.use(bodyParser.json());
 
 // app.use(cookieParser()); ??
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 
 
@@ -37,24 +37,24 @@ app.use(passport.initialize());
 
 app.use(flash());
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, 'client/build/')));
-//   // Handle React routing, return all requests to React app
-//   app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// }
-
-// Directs Heroku to the right page on the build version
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
 // Routes
 // ========================================================
 app.use('/user', user);
 app.use('/api', api);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build/')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+// Directs Heroku to the right page on the build version
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
