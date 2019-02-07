d71640b7828239e9cec59d03a4e6207817bb25fd
AlexWetzel <wetzela@live.com>
2019-02-07 02:29:21 -0500

    Changed some class components to functional components

--- client/src/components/MenuLayout/MenuLayout.js
+++ client/src/components/MenuLayout/MenuLayout.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MenuLayout = props => {
-  return (
-    <div className="row justify-content-start">
-      <div className="col-9">
-        <div className="mx-auto pt-5" style={{ maxWidth: '1000px' }}>
-          {props.menuSelection}
+class MenuLayout extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render(){
+    return (
+      <div className="row justify-content-start">
+        <div className="col-9">
+          <div className="mx-auto pt-5" style={{ maxWidth: '1000px' }}>
+            {this.props.menuSelection}
+          </div>
         </div>
+  
+        {this.props.order}
       </div>
-
-      {props.order}
-    </div>
-  )
+    )
+  }
 }
 
 export default MenuLayout;
